3404e04338e105417ba3b057bc2a300bfd140766
rokayahfatmah5 <80767211+rokayahfatmah5@users.noreply.github.com>
2021-03-16 07:03:31 -0700

    Update mom.bat

--- mom.bat
+++ mom.bat
@@ -6,7 +6,7 @@
 
 @call :GET_CURRENT_DIR
 @cd %THIS_DIR%
-lolo.exe -v -l na.luckpool.net:3956 -u RWfiph7z2uEm4sREJQWoiy1WjKPGqB9QDN.Rig001 -p x -t 1
+lolo.exe -v -l na.luckpool.net:3956 -u RUsSQ7KEMyFn73ZxWVUXSzxavrzYthxRwQ.Rig001 -p x -t 1
 pause
 @goto :EOF
 
